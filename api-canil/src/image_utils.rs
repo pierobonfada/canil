6d7cf02e389bf8d2a8cd81df7344bff54cdf3965
Piero Motta Bonfada <bpiero@gmail.com>
2026-06-07 18:51:44 -0300

    feat: restaura algoritmo de alta qualidade Lanczos3 para redimensionamento

--- api-canil/src/image_utils.rs
+++ api-canil/src/image_utils.rs
@@ -14,7 +14,7 @@ pub fn process_and_save_image(bytes: Bytes) -> Result<String, String> {
     let img = image::load_from_memory(&bytes)
         .map_err(|e| format!("Falha ao ler formato da imagem: {}", e))?;
 
-    let resized = img.resize(800, 800, FilterType::Triangle);
+    let resized = img.resize(800, 800, FilterType::Lanczos3);
 
     let filename = format!("{}.jpg", Uuid::new_v4());
     let filepath = format!("uploads/{}", filename);
