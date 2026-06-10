122e023d69e83cb99c02ceb20d98a77453d8c953
Piero Motta Bonfada <bpiero@gmail.com>
2026-06-10 15:16:42 -0300

    replace text toggle with svg eye icons

--- admin-canil/src/views/ChangePasswordView.vue
+++ admin-canil/src/views/ChangePasswordView.vue
@@ -17,7 +17,8 @@
               minlength="10"
             />
             <button type="button" class="btn-toggle-password" @click="showPassword = !showPassword" title="Mostrar/Ocultar senha">
-              <span style="font-size: 0.8rem; font-weight: bold; color: #475569;">{{ showPassword ? 'OCULTAR' : 'MOSTRAR' }}</span>
+              <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="color: #64748b;"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="color: #64748b;"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
             </button>
           </div>
           <p class="password-hint">
@@ -159,7 +160,7 @@ button:disabled { background-color: #9ca3af; cursor: not-allowed; }
 .password-input-wrapper input {
   width: 100%;
   box-sizing: border-box;
-  padding-right: 70px;
+  padding-right: 40px;
 }
 .btn-toggle-password {
   position: absolute;
--- admin-canil/src/views/DashboardView.vue
+++ admin-canil/src/views/DashboardView.vue
@@ -141,7 +141,8 @@ permissão de Master ('v-if="isMaster"'). -->
             <div class="password-input-wrapper">
               <input :type="showPassword ? 'text' : 'password'" v-model="editProfileData.password" placeholder="Manter inalterada" />
               <button type="button" class="btn-toggle-password" @click="showPassword = !showPassword" title="Mostrar/Ocultar senha">
-                <span style="font-size: 0.8rem; font-weight: bold; color: #475569;">{{ showPassword ? 'OCULTAR' : 'MOSTRAR' }}</span>
+                <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="color: #64748b;"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="color: #64748b;"><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path><line x1="1" y1="1" x2="23" y2="23"></line></svg>
               </button>
             </div>
             <p class="password-hint" v-if="editProfileData.password.length > 0">
@@ -595,7 +596,7 @@ button { padding: 0.8rem; border: none; border-radius: 8px; font-weight: bold; c
 .password-input-wrapper input {
   width: 100%;
   box-sizing: border-box;
-  padding-right: 70px;
+  padding-right: 40px;
 }
 .btn-toggle-password {
   position: absolute;
